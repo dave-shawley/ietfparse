558322f71c59c1fc810b87c6ad87d010d7a6c38d
Dave Shawley <daveshawley@gmail.com>
2014-09-20 23:22:22 -0400

    setup.py: Implement a useful `clean` command.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+from distutils import dir_util
 import codecs
+import os.path
 import setuptools
 import sys
 
@@ -33,6 +35,47 @@ with codecs.open('README.rst', 'rb', encoding='utf-8') as file_obj:
     long_description = '\n' + file_obj.read()
 
 
+class Janitor(setuptools.Command):
+    _top_dir = os.path.abspath(os.path.dirname(__file__))
+    description = 'remove generated and/or temporary files'
+    user_options = [
+        ('all', 'a', 'remove all generated files'),
+        ('dist', 'd', 'remove distribution directory'),
+        ('eggs', 'e', 'remove egg and egg-info directories'),
+        ('pycache', 'p', 'remove __pycache__ directories'),
+        ('environment', 'E', 'remove environment'),
+    ]
+    boolean_options = ['all', 'dist', 'eggs', 'pycache', 'environment']
+
+    def initialize_options(self):
+        for name in self.boolean_options:
+            setattr(self, name, False)
+
+    def finalize_options(self):
+        if self.all:
+            for name in self.boolean_options:
+                setattr(self, name, True)
+
+    def run(self):
+        self._remove_tree('build')
+        if self.dist:
+            self._remove_tree('dist')
+        if self.environment:
+            self._remove_tree('env')
+        if self.eggs:
+            for name in os.listdir(self._top_dir):
+                if name.endswith('.egg') or name.endswith('.egg-info'):
+                    self._remove_tree(name)
+        if self.pycache:
+            for root, dirs, _ in os.walk(self._top_dir):
+                if '__pycache__' in dirs:
+                    self._remove_tree(os.path.join(root, '__pycache__'))
+
+    def _remove_tree(self, sub_dir):
+        dir_util.remove_tree(os.path.join(self._top_dir, sub_dir),
+                             verbose=self.verbose, dry_run=self.dry_run)
+
+
 setuptools.setup(
     name='ietfparse',
     version=__version__,
@@ -59,4 +102,5 @@ setuptools.setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Text Processing',
     ],
+    cmdclass={'clean': Janitor}
 )
