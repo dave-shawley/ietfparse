fe460a7594ce708fe566f5be8fe7aa5b22c3f950
Dave Shawley <daveshawley@gmail.com>
2017-12-24 13:08:02 -0500

    docs: Switch to alabaster.

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sphinx_rtd_theme
-
 import ietfparse
 
 
@@ -23,10 +21,16 @@ source_suffix = '.rst'
 source_encoding = 'utf-8-sig'
 master_doc = 'index'
 pygments_style = 'sphinx'
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = []
 exclude_patterns = []
+html_sidebars = {
+    '**': ['about.html', 'navigation.html', 'searchbox.html'],
+}
+html_theme_options = {
+    'github_user': 'dave-shawley',
+    'github_repo': 'ietfparse',
+    'github_banner': True,
+}
 
 intersphinx_mapping = {
     'python': ('http://docs.python.org/3/', None),
