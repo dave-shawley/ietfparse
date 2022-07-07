87c039dec966effb65ee346407e3bac77cf67f50
Dave Shawley <daveshawley@gmail.com>
2022-07-07 06:39:38 -0400

    Reorganize docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,19 +3,13 @@
 
 import ietfparse
 
-
 project = 'ietfparse'
-copyright = '2014-2020, Dave Shawley'
+copyright = '2014-2022, Dave Shawley'
 version = ietfparse.version
 release = '.'.join(str(x) for x in ietfparse.version_info[:2])
 
 needs_sphinx = '1.0'
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode',
-    'sphinxcontrib.httpdomain',
-]
+extensions = []
 templates_path = []
 source_suffix = '.rst'
 source_encoding = 'utf-8-sig'
@@ -32,6 +26,17 @@ html_theme_options = {
     'github_banner': True,
 }
 
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
+extensions.append('sphinx.ext.autodoc')
+
+# https://www.sphinx-doc.org/en/master/usage/extensions/viewcode.html
+extensions.append('sphinx.ext.viewcode')
+
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
+extensions.append('sphinx.ext.intersphinx')
 intersphinx_mapping = {
     'python': ('http://docs.python.org/3/', None),
 }
+
+# https://sphinxcontrib-httpdomain.readthedocs.io/
+extensions.append('sphinxcontrib.httpdomain')
